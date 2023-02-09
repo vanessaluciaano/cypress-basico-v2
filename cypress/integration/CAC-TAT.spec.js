75078db5fd0f3a9494a94cc153149f1cb17cc9dd
vanessaluciaano <vanessa.lucianav@gmail.com>
2023-02-09 13:54:39 -0300

    Marcando inputs do tipo radio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -96,10 +96,27 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('have.value', 'mentoria')
     })
 
-    it.only('selecione um produto (blog) pelo seu indice', function() {
+    it('selecione um produto (blog) pelo seu indice', function() {
         cy.get('#product')
         .select(1)
         .should('have.value', 'blog')
     })
+
+    it('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should('have.value', 'feedback')
+    })
+
+    it('marca cada tipo de atendimento', function() {
+        cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(function($radio){
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        })
+    })
+
+    
   })
   
