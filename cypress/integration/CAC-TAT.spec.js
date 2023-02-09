75f3122e54fe55e712ee0fbca512dc75232cd90f
vanessaluciaano <vanessa.lucianav@gmail.com>
2023-02-09 16:07:37 -0300

    Lidando com links que abrem em outra aba

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -153,5 +153,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         })
     })
 
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+
+  
   })
   
